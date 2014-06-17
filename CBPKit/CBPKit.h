9be02c48298cfb92fb04a4d5f1b1f04f4ce4c416
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-06-17 10:44:14 +0100

    Created frameworks
    Two frameworks here - 1 for all the pets of the app that can be shared
    with extensions and one for SDWebImage so it can be used by both.

---
+++ CBPKit/CBPKit.h
@@ -0,0 +1,17 @@
+//
+//  CBPKit.h
+//  CBPKit
+//
+//  Created by Karl Monaghan on 14/06/2014.
+//  Copyright (c) 2014 Crayons and Brown Paper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CBPKit.
+FOUNDATION_EXPORT double CBPKitVersionNumber;
+
+//! Project version string for CBPKit.
+FOUNDATION_EXPORT const unsigned char CBPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBPKit/PublicHeader.h>
---
+++ SDWebImageKit/SDWebImageKit.h
@@ -0,0 +1,33 @@
+//
+//  SDWebImageKit.h
+//  SDWebImageKit
+//
+//  Created by Karl Monaghan on 15/06/2014.
+//  Copyright (c) 2014 Crayons and Brown Paper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDWebImageKit.
+FOUNDATION_EXPORT double SDWebImageKitVersionNumber;
+
+//! Project version string for SDWebImageKit.
+FOUNDATION_EXPORT const unsigned char SDWebImageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDWebImageKit/PublicHeader.h>
+
+#import <SDWebImageKit/MKAnnotationView+WebCache.h>
+#import <SDWebImageKit/NSData+ImageContentType.h>
+#import <SDWebImageKit/SDImageCache.h>
+#import <SDWebImageKit/SDWebImageCompat.h>
+#import <SDWebImageKit/SDWebImageDecoder.h>
+#import <SDWebImageKit/SDWebImageDownloader.h>
+#import <SDWebImageKit/SDWebImageDownloaderOperation.h>
+#import <SDWebImageKit/SDWebImageManager.h>
+#import <SDWebImageKit/SDWebImagePrefetcher.h>
+#import <SDWebImageKit/UIButton+WebCache.h>
+#import <SDWebImageKit/UIImage+GIF.h>
+#import <SDWebImageKit/UIImage+MultiFormat.h>
+#import <SDWebImageKit/UIImage+WebP.h>
+#import <SDWebImageKit/UIImageView+HighlightedWebCache.h>
+#import <SDWebImageKit/UIImageView+WebCache.h>
